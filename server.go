d6ea1d7ad14f8fa49b6e5fa90a9d85013392ccb3
duncan odhiambo <45081961+duncanodhis@users.noreply.github.com>
2022-10-14 13:26:25 +0300

    Change fileserver directory
    The directory in fileserver should be changed to where the files to be served are located

--- server.go
+++ server.go
@@ -8,4 +8,5 @@ import (
 func main() {
 	// Simple static webserver:
 	log.Fatal(http.ListenAndServe(":8080", http.FileServer(http.Dir("/home/tonycark/Desktop/acronis/find"))))
+	//change /home/tonycark/Desktop/acronis/find to the directory the files are located
 }
